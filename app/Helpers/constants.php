df15bf8b926b5639ec040bb28d95db90b0b8da63
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-08-10 14:26:50 +0300

    Add 'constants' file

---
+++ app/Helpers/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+// Default API constants
+const API_URL = "http://newspaperapi.test/";
+const DEFAULT_HEADERS = [
+    "Content-Type: application/json"
+];
+
+// HTTP Verbs
+const GET = "GET";
+const POST = "POST";
+const PUT = "PUT";
