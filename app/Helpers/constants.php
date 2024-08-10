7d296886cd86376e3ccc63d353931ddd2270750b
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-08-10 15:59:54 +0300

    Fix default headers

--- app/Helpers/constants.php
+++ app/Helpers/constants.php
@@ -1,9 +1,9 @@
 <?php
 
 // Default API constants
-const API_URL = "http://newspaperapi.test/";
+const API_URL = "http://newspaperapi.test/api/v1/";
 const DEFAULT_HEADERS = [
-    "Content-Type: application/json"
+    "Content-Type"=>"application/json"
 ];
 
 // HTTP Verbs
