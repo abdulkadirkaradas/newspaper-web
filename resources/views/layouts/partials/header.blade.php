e3461c03dc71b1a5cacb8d47e6b6cfa5b89fe7b8
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-03 15:27:34 +0300

    Add header file

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="#" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                <span>{{ env('APP_NAME') }}</span>
+            </a>
+
+            <div class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                {{-- TODO: Categories will be added in the future --}}
+            </div>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search" />
+            </form>
+
+            <div class="text-end">
+                {{-- TODO: Login should be pop-up so that user can easily log in --}}
+                <a href="#" class="btn btn-outline-light me-2">Login</a>
+                {{-- TODO: Register process must be maded with pop-up --}}
+                <a href="{{ route('register.view') }}" class="btn btn-warning">Sign-up</a>
+            </div>
+        </div>
+    </div>
+</header>
+<p></p>
