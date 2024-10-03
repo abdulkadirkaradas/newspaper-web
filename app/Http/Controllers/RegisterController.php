7805352b7fe4e52e7c4435e7f7788c68cf47a386
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-03 15:21:56 +0300

    Add view and register functions to the RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ApiCaller;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    private ApiCaller $apiCaller;
+
+    public function __construct()
+    {
+        $this->apiCaller = new ApiCaller(API_URL, DEFAULT_HEADERS);
+    }
+
+    public function view()
+    {
+        return view('layouts.auth.register.register');
+    }
+
+    public function register(Request $request)
+    {
+        $params = $request->only(['name', 'lastname', 'username', 'email', 'password']);
+
+        $this->apiCaller->call(POST, 'auth/register/', [], [
+            "name" => $params['name'],
+            "lastname" => $params['lastname'],
+            "username" => $params['username'],
+            "email" => $params['email'],
+            "password" => $params['password'],
+        ]);
+
+        $response = $this->apiCaller->getResponse();
+
+        return view('layouts.main', ['response' => $response]);
+    }
 }
