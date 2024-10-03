ec1486f2be93f36ba9767084cdcb11921d9f0c42
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-03 15:23:34 +0300

    Add register lang file

---
+++ lang/en/register.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "title" => "Register and discuss with the people",
+    "name" => "Name",
+    "lastname" => "Lastname",
+    "username" => "Username",
+    "email" => "Email",
+    "password" => "Password",
+    "emailInput" => "Enter email",
+    "submit" => "Submit",
+];
