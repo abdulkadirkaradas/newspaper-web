1e51e0d86bdf4fa258fac3cf559b66f8cd39d686
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-03 15:23:13 +0300

    Add comment

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'News'), // This name will be changed with a proper name
 
     /*
     |--------------------------------------------------------------------------
