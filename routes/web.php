d0b71975165ad2de2a9734417d0cebcddbfa7a27
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-03 15:22:49 +0300

    Add view and register functions routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'dashboard']);
+
+Route::prefix('register')->group(function () {
+    Route::get('/', [RegisterController::class, 'view'])->name('register.view');
+    Route::post('/save', [RegisterController::class, 'register'])->name('register.save');
+});
