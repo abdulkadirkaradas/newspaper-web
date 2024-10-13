e5e034f47c789983d956734fce70cf953aab390e
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-13 15:28:24 +0300

    Add alias name to the dashboard route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'dashboard']);
+Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
 
 Route::prefix('register')->group(function () {
     Route::get('/', [RegisterController::class, 'view'])->name('register.view');
