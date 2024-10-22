b00ee02414e6c8f01c752f48e15a8b9b8c1d381e
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-22 15:02:39 +0300

    Add fontawesome library cdn

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -5,6 +5,9 @@
 <!-- Bootstrap Styles -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
+{{-- Fontawesome Library --}}
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet"/>
+
 <!-- Bootstrap Libraries -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
