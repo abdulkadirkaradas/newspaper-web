4262be6d29654cb772f3371a80c20c68f33bcec5
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-22 16:32:58 +0300

    Add login web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,9 @@ Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
 Route::prefix('register')->group(function () {
     Route::get('/', [RegisterController::class, 'view'])->name('register.view');
     Route::post('/save', [RegisterController::class, 'register'])->name('register.save');
+});
+
+Route::prefix('login')->group(function () {
+    Route::get('/', [LoginController::class, 'view'])->name('login.view');
+    Route::post('/login', [LoginController::class, 'login'])->name('login.login');
 });
