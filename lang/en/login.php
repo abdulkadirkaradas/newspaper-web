eaa22f24931141a97894a28a7529cfc672124e2a
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-22 16:32:19 +0300

    Add login lang file

---
+++ lang/en/login.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "title" => "Login and discuss with the people",
+    "email" => "Email",
+    "password" => "Password",
+    "emailInput" => "Enter email",
+    "submit" => "Login",
+];
