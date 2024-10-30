35b78de5483c7dfe0a9f858075bd57113f9e62e1
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-30 17:28:44 +0300

    Add commonFunctions file. Add 'convertUTCDateToLocalDate' function. Move 'render_page' function to commonFunction file. Remove ViewHelper file.

--- app/Helpers/commonFunctions.php
+++ app/Helpers/commonFunctions.php
@@ -1,5 +1,14 @@
 <?php
 
+use Carbon\Carbon;
+
+if (!function_exists('convertUTCDateToLocalDate')) {
+    function convertUTCDateToLocalDate($date)
+    {
+        return Carbon::parse($date)->format('d M Y');
+    }
+}
+
 if (!function_exists('render_page')) {
     function render_page($view, $data = [])
     {
