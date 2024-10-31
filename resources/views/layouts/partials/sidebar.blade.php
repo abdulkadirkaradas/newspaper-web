604ebddf0f9bceb9f5ce8869d5099b9b5f07fbe9
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-10-31 15:11:59 +0300

    Add sidebar resize function

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,3 +1,14 @@
-<aside class="sidebar text-white bg-dark">
-
+<aside class="sidebar text-white bg-dark" style="position: fixed; left: 0; margin-top: 70px;">
 </aside>
+
+<script>
+    $(document).ready(setSidebarMargin);
+    $(window).on('resize', setSidebarMargin);
+
+    function setSidebarMargin() {
+        const header = $('header');
+        const sidebar = $('aside');
+
+        sidebar.css('margin-top', header.outerHeight());
+    }
+</script>
