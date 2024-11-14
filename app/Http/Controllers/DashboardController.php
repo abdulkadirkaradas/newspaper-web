09bf109846f10980eac2afc63b65eeea452cc2a4
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-11-14 15:18:06 +0300

    Improve dashboard function. Fix usage of apiCaller and apiHeader class instances.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,23 +3,43 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ApiCaller;
-use Illuminate\Http\Request;
+use App\Helpers\ApiHeaders;
 
 class DashboardController extends Controller
 {
     private ApiCaller $apiCaller;
+    private ApiHeaders $apiHeaders;
 
-    public function __construct()
+    public function __construct(ApiHeaders $apiHeaders)
     {
-        $this->apiCaller = new ApiCaller(API_URL, DEFAULT_HEADERS);
+        $this->apiHeaders = $apiHeaders;
+
+        $this->apiCaller = new ApiCaller(
+            $this->apiHeaders->getApiUrl(),
+            $this->apiHeaders->getHeaders()
+        );
     }
 
     public function dashboard()
     {
         //TODO: https://github.com/abdulkadirkaradas/newspaper-web/issues/3
-        $this->apiCaller->call(GET, 'public/news/', [], [
-            "type" => "all"
-        ]);
+
+        // Added for testing purposes
+        // session()->forget('auth_token');
+
+        $endpoint = session()->exists('auth_token')
+            ? 'writer/news/logged-user-news/'
+            : 'public/news/';
+
+        $query = session()->exists('auth_token')
+            ? []
+            : ["type" => "all"];
+
+        $userInformation = session()->exists('auth_token')
+            ? getUserInformation()
+            : [];
+
+        $this->apiCaller->call(GET, $endpoint, [], $query);
 
         $response = $this->apiCaller->getResponse();
         $decoded = json_decode($response, true);
@@ -27,7 +47,16 @@ class DashboardController extends Controller
 
         $newsCategories = $this->getNewsCategories();
 
-        return view('layouts.main', ['news' => $news, 'newsCategories' => $newsCategories]);
+        $data = [
+            'news' => $news,
+            'newsCategories' => $newsCategories
+        ];
+
+        if ($userInformation !== []) {
+            $data['userInformation'] = $userInformation;
+        }
+
+        return view('layouts.main', $data);
     }
 
     private function getNewsCategories()
