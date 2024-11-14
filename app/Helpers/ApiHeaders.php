fa7d9380e08ae43b157bde61b484b6bd3ecdeb3f
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-11-14 15:16:38 +0300

    Fix bearer token set function. Improve ApiHeaders helper file.

--- app/Helpers/ApiHeaders.php
+++ app/Helpers/ApiHeaders.php
@@ -3,30 +3,35 @@
 namespace App\Helpers;
 
 class ApiHeaders {
-    private static string $API_URL = "http://newspaperapi.test/api/v1/";
-    private static array $headers = [
+    private string $API_URL = "http://newspaperapi.test/api/v1/";
+    private array $headers = [
         "Content-Type" => "application/json",
     ];
 
-    public static function setApiUrl(string $apiUrl = null): void {
-        self::$API_URL = $apiUrl ?? self::$API_URL;
+    public function setApiUrl(?string $apiUrl = null): void {
+        $this->API_URL = $apiUrl ?? $this->API_URL;
     }
 
-    public static function getApiUrl(): string {
-        return self::$API_URL;
+    public function getApiUrl(): string {
+        return $this->API_URL;
     }
 
-    public static function setHeaders(array $headers = null): void {
-        self::$headers = $headers ? array_merge(self::$headers, $headers) : self::$headers;
+    public function setHeaders(?array $headers = null): void {
+        if ($headers === null) {
+            throw new \InvalidArgumentException("Headers cannot be null.");
+        }
+        $this->headers = array_merge($this->headers, $headers);
     }
 
-    public static function getHeaders(): array {
-        return self::$headers;
+    public function getHeaders(): array {
+        $this->setBearerToken();
+
+        return $this->headers;
     }
 
-    public static function setBearerToken(string $headerName = 'Authorization', string $bearerToken = null): void {
-        if ($bearerToken !== null) {
-            self::$headers[$headerName] = "Bearer $bearerToken";
+    private function setBearerToken(): void {
+        if (session()->exists('auth_token')) {
+            $this->headers['Authorization'] = "Bearer " . session('auth_token');
         }
     }
 }
